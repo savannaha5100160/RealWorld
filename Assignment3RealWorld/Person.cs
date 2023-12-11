97caac0c596693283fc46126f88f7f2ce6c95357
savannaha5100160 <5100160@mypillar7uwc.ac.za>
2023-12-11 13:07:05 +0200

    first commit

---
+++ Assignment3RealWorld/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3RealWorld
+{
+     public class Person
+    {
+        private string firstName;
+        private string LastName;
+        private int age;
+        private string Location;
+        private string occupation;
+        private string hobbies;
+
+        public Person (string firstName, string LastName, int age, string Location, string occupation, string hobbies)
+        {
+            this.firstName = firstName;
+            this.LastName = LastName;
+            this.age = age;
+            this.Location = Location;
+            this.occupation = occupation;
+            this.hobbies = hobbies;
+        }
+        public void Introduce ()
+        {
+            Console.WriteLine($"Hello, my name is {firstName} {LastName}, I am {age} years old, and I am from {Location}. I work as a {occupation}. My hobbies include {hobbies}.");
+        }
+    }
+ 
+}
---
+++ Assignment3RealWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment3RealWorld
+{
+     class Program
+    {
+        static void Main( )
+        {
+            Person Oliver = new Person ("Oliver", "James", 35, "Strand", "Software Developer", "coding and reading");
+            Oliver.Introduce();
+
+            Person Madison = new Person("Madison", "West", 29, "Mossel Bay", "Teacher", "Rugby and cooking");
+            Madison.Introduce();
+
+            Person Scott = new Person("Scott", "Evans", 40, "George", "Doctor", "Writing and Mountain biking");
+            Scott.Introduce();
+
+            Person Rose = new Person("Rose", "Rivera", 25, "Strand", "Nurse", "Singing and crochet");
+            Rose.Introduce();
+
+            Person Ashley = new Person("Ashley", "Rivera", 27, "Strand", "Teacher", "Soccer and painting");
+            Ashley.Introduce();
+
+            Person Hunter = new Person("Hunter", "Murray", 30, "Worcester", "Graphic Designer", "Learning and drawing");
+            Hunter.Introduce();
+        }
+    }
+}
